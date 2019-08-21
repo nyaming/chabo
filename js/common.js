c9b281b0df833cdf7f8bd4870bc68fbf1edcde18
nyaming <43199895+nyaming@users.noreply.github.com>
2019-08-22 00:56:28 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -22,30 +22,14 @@
 		  }
 		});
 	  });
-	  
-	//   $(window).scroll(function (){
-	// 	$(".js-scroll-blur").each(function(){
-	// 	  var imgPos = $(this).offset().top;    
-	// 	  var scroll = $(window).scrollTop();
-	// 	  var windowHeight = $(window).height();
-	// 	  if (scroll > imgPos - windowHeight +200){
-	// 		$(this).addClass("js-scroll-blur-in");
-	// 	  }
-	// 	});
-	//   });
+	 $(window).scroll(function (){
+		$(".js-scroll-up").each(function(){
+		  var imgPos = $(this).offset().top;    
+		  var scroll = $(window).scrollTop();
+		  var windowHeight = $(window).height();
+		  if (scroll > imgPos - windowHeight +150){
+			$(this).addClass("js-scroll-up-in");
+		  }
+		});
+	  });
  });
-
-/*スクロールアクション*/	
-// $(window).scroll(function (){
-//     $(".effect").each(function(){
-//       var imgPos = $(this).offset().top;    
-//       var scroll = $(window).scrollTop();
-//       var windowHeight = $(window).height();
-//       if (scroll > imgPos - windowHeight + windowHeight/5 + 50){
-//         $(this).addClass("effect-in");
-//       } else {
-        
-// 		$(this).removeClass("effect-in");
-//       }
-//     });
-//   });
