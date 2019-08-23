bfc148ba38e03f343e0d79a4fa8a27e972409e02
nyaming <43199895+nyaming@users.noreply.github.com>
2019-08-23 22:57:41 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
  ////////* スクロールアクション *////////
- $(function() {
+$(function() {
 	 
 	 $(window).scroll(function (){
 		$(".js-scroll").each(function(){
@@ -13,7 +13,7 @@
 	  });
 	 
 	 $(window).scroll(function (){
-		$(".js-scroll-slow").each(function(){
+		$(".js-scroll-stay").each(function(){
 		  var imgPos = $(this).offset().top;    
 		  var scroll = $(window).scrollTop();
 		  var windowHeight = $(window).height();
@@ -32,4 +32,34 @@
 		  }
 		});
 	  });
- });
+});
+
+////////* SP ドロワーメニュー *//////// 
+$("#sp_menu_btn").on('click', function(){
+	$('.header_nav').fadeIn(0);
+	$(this).toggleClass('open');
+	$('.header_nav').toggleClass('open');
+	if ($('#sp_menu_btn').hasClass('open')) {
+		setTimeout(function () {
+			$('.header_nav').removeClass('close');
+			$('.nav_btn_txt').html('CLOSE');
+		}, 0);
+		// スクロールを無効にする
+		$('body,html').css({"overflow":"hidden","height":"100%"});
+	}
+	else{
+		setTimeout(function () {
+			$('.header_nav').addClass('close');
+			$('.header_nav').fadeOut();
+			$('.nav_btn_txt').html('MENU');
+		}, 0);
+		// スクロール無効を解除する
+		$('body,html').css({"overflow":"visible","height":"auto"});
+	}
+})
+$('.header_nav a').click (function(){
+	// スクロール無効を解除する
+	$('body,html').css({"overflow":"visible","height":"auto"});
+});
+
+
